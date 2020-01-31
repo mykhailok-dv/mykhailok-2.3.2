ead5ee91e7dbe015745fbecacc10306e2c9be797
Mykhailo Khrypko <mykhailok@default-value.com>
2020-01-31 15:00:40 +0200

    task-46557: fix whitespace

--- app/code/Mykhailok/SupportChat/view/frontend/requirejs-config.js
+++ app/code/Mykhailok/SupportChat/view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     map: {
         '*': {
             mykhailokSupportChatActionSave:     'Mykhailok_SupportChat/js/action/save-message',
-            mykhailokSupportChatModelChat:   'Mykhailok_SupportChat/js/model/chat',
+            mykhailokSupportChatModelChat:      'Mykhailok_SupportChat/js/model/chat',
             mykhailokSupportChatViewChat:       'Mykhailok_SupportChat/js/view/chat',
             mykhailokSupportChatOpenButton:     'Mykhailok_SupportChat/js/open-button',
             mykhailokSupportChatToggleChat:     'Mykhailok_SupportChat/js/toggle-chat',
